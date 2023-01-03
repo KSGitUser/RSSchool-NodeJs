4fcb51d8ccd8c63a17d8d7781d66b59cad07fab0
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-01-03 21:09:26 +0300

    feat: test with jest and babel

--- assignments/crud-api/tests/get.test.ts
+++ assignments/crud-api/tests/get.test.ts
@@ -1,17 +1,16 @@
 /// <reference lib="dom" />
-import { describe, it, before, after} from "node:test";
-import * as assert from "assert";
-import {startServer, closeServer} from "../src/start-server.js";
+import { describe, it, beforeAll, afterAll, expect} from '@jest/globals';
+import {startServer, closeServer} from "../src/start-server";
 
-describe('Test API',  async () => {
-    before(() => startServer());
+describe('Test API',   () => {
+    beforeAll(() => startServer());
 
      it('Get users', async ()=> {
         try {
             const user = await fetch('http://localhost:3000/api/users').then(async data => {
                 return await data.json();
             })
-            assert.deepStrictEqual(user, []);
+            expect(user).toMatchObject([])
         } catch(error) {
             throw error;
         }
@@ -28,13 +27,13 @@ describe('Test API',  async () => {
             const createdUser = await fetch('http://localhost:3000/api/users', { method: 'POST', body: JSON.stringify(mockedUser)}).then(async data => {
                 return await data.json();
             })
-            assert.ok(createdUser.hasOwnProperty('id'));
+            expect(createdUser).toHaveProperty('id');
             const userForTest = { username: createdUser.username, age: createdUser.age, hobbies: createdUser.hobbies};
-            assert.deepStrictEqual(mockedUser, userForTest);
+            expect(mockedUser).toMatchObject(userForTest);
         } catch(error) {
             throw error;
         }
     })
 
-    after(()=>closeServer())
+    afterAll(()=>closeServer())
 });
