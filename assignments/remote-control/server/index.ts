982b2b89538fa8023eaebfa34cf73b4769f73a85
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-01-22 20:17:08 +0300

    feature: print commands on client

--- assignments/remote-control/server/index.ts
+++ assignments/remote-control/server/index.ts
@@ -1,7 +1,15 @@
 import { WebSocketServer, createWebSocketStream } from "ws";
 import { nutFunctions, parseCommands } from "./src/nut-commands.js";
 
-const wss = new WebSocketServer({ port: 8080 });
+const port = 8080;
+
+const onServerCreated = () => {
+  setTimeout(() => {
+    console.log(`Server created on ws://localhost:${port}`);
+  });
+};
+
+const wss = new WebSocketServer({ port: port }, onServerCreated);
 
 wss.on("connection", function connection(ws) {
   const stream = createWebSocketStream(ws, {
--- assignments/remote-control/server/src/draw-figures.ts
+++ assignments/remote-control/server/src/draw-figures.ts
@@ -1,11 +1,13 @@
 import { down, left, mouse, Point, right, up } from "@nut-tree/nut-js";
 import { moveMouseRight } from "./move-cursor.js";
+import { COMMANDS } from "./consts.js";
 
-export const drawRectangle = async (...args: number[]): Promise<void> => {
+export const drawRectangle = async (...args: number[]): Promise<string> => {
   let [width, height] = args;
   if (!width && !height) {
-    return;
+    return "";
   }
+  const commandName = !height ? COMMANDS.draw_square : COMMANDS.draw_rectangle;
 
   if (width && !height) {
     height = width;
@@ -14,12 +16,13 @@ export const drawRectangle = async (...args: number[]): Promise<void> => {
   await mouse.drag(down(height as number));
   await mouse.drag(left(width as number));
   await mouse.drag(up(height as number));
+  return commandName;
 };
 
-export const drawCircle = async (...args: number[]): Promise<void> => {
+export const drawCircle = async (...args: number[]): Promise<string> => {
   const [radius] = args;
   if (!radius) {
-    return;
+    return "";
   }
   const { x: centerX, y: centerY } = await mouse.getPosition();
 
@@ -35,4 +38,5 @@ export const drawCircle = async (...args: number[]): Promise<void> => {
     circlePath.push(new Point(x, y));
   }
   await mouse.drag(circlePath);
+  return COMMANDS.draw_circle;
 };
