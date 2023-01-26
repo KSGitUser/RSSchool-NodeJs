82a8b0240f2203a51b2b8b66c4de52f0e38b304e
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-01-26 03:21:11 +0300

    feat: add member-types ts

--- assignments/graphql-service/src/routes/member-types/index.ts
+++ assignments/graphql-service/src/routes/member-types/index.ts
@@ -8,7 +8,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity | null> {
+      return (
+        fastify.db.memberTypes.findOne({
+          key: 'id',
+          equals: request.params.id,
+        }) ?? reply.notFound(`No member type with ${request.params.id}`)
+      );
+    }
   );
 
   fastify.patch(
@@ -28,7 +37,13 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity | undefined> {
+      try {
+        return fastify.db.memberTypes.change(request.params.id, request.body);
+      } catch (e) {
+        reply.notFound(`No member type with ${request.params.id}`);
+      }
+    }
   );
 };
 
